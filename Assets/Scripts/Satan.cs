1f0bb85675e33731af577595d8afbdc8654dc56b
Unknown <nersonnils@gmail.com>
2019-02-05 21:52:49 +0100

    small fx

--- Assets/Scripts/Satan.cs
+++ Assets/Scripts/Satan.cs
@@ -7,6 +7,19 @@ namespace GRP04.SatanAssistant
     public class Satan : Entity
     {
         [SerializeField] private Animator anim;
+        private Vector3 pos;
+
+        protected override void Start()
+        {
+            base.Start();
+            pos = transform.position;
+        }
+
+        protected override void Update()
+        {
+            base.Update();
+            transform.position = pos;
+        }
 
         public override void OnSoulJudgedRight()
         {
